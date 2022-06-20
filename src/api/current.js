062312af3ab7c728385f423b4a103d2864f87c82
angel <angelhidalgomb@gmail.com>
2022-06-20 03:05:30 -0500

    Add markup and template content

--- src/api/current.js
+++ src/api/current.js
@@ -1,9 +1,10 @@
 const API_KEY = '4aca6f2bce164fffb4741737221906';
 const BASE_URL = 'http://api.weatherapi.com/v1';
+const searchQuery = document.querySelector('#search');
 
 const getCurrentWeather = async () => {
   try {
-    const requestURL = `${BASE_URL}/current.json?key=${API_KEY}&aqi=yes&q=mexico city`;
+    const requestURL = `${BASE_URL}/current.json?key=${API_KEY}&aqi=yes&q=${searchQuery.value}`;
     const response = await fetch(requestURL);
     const data = await response.json();
     return data;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import './style.css';
+import './icons/weather/weather-icons.css';
 import { getCurrentWeather } from './api/current';
 import { parseCurrentWeather } from './api/parseCurrent';
 // import { current } from './api/current';
 
-parseCurrentWeather().then((data) => console.log(data.country));
+const form = document.querySelector('form');
+const querySearch = document.querySelector('#search');
+
+const getDataObject = async () => {
+  const data = await parseCurrentWeather();
+  console.log(data);
+};
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  getDataObject();
+});
